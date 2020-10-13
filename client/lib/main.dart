2ed15f22bd98e6e99e9d870e56d350bcadb3d41b
oi73ooh <cindian@amazingbutuseless.net>
2020-10-14 00:15:52 +0900

    웹어플리케이션에서 iframe을 사용해서 화면에 유튜브 동영상을 표시 할 수 있는 기능 추가

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,12 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io' as io;
+import 'dart:html';
+import 'dart:ui' as ui;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,10 +19,55 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('ijustwannaseewonwoo'),
         ),
-        body: Center(
-          child: Text('원우야 보고 싶다.'),
-        ),
+        body: WebViewExample(),
       ),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller = Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (kIsWeb) {
+      final IFrameElement _iframeElement = IFrameElement();
+      _iframeElement.src = 'https://www.youtube.com./embed/3-lz0ejzadM';
+
+      ui.platformViewRegistry.registerViewFactory('iframeElement', (int viewId) => _iframeElement);
+
+    } else if (io.Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  Widget _iframeWidget = HtmlElementView(
+    key: UniqueKey(),
+    viewType: 'iframeElement',
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb) {
+      final _width = MediaQuery.of(context).size.width;
+      final _height = _width * (9 / 16);
+
+      return SizedBox(
+        width: _width,
+        height: _height,
+        child: _iframeWidget,
+      );
+    }
+
+    return WebView(
+      initialUrl: 'https:/flutter.dev',
     );
   }
 }

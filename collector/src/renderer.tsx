b9f0c498ab434a0cc55ee07dfbb25cdd8b96ac2e
amazingbutuseless <cindian@amazingbutuseless.net>
2020-12-05 01:20:04 +0900

    :wrench: Redux 사용을 위한 설정 추가

--- collector/src/renderer.tsx
+++ collector/src/renderer.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { hot } from "react-hot-loader";
+import { hot } from 'react-hot-loader';
+
+import { Provider } from 'react-redux';
+import Store from './store';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+ReactDOM.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
 
 export default hot(module)(App);
---
+++ collector/src/store.ts
@@ -0,0 +1,7 @@
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;

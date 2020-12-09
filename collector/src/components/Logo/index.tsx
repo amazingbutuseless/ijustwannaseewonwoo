9547a7f632afd6213607a976b922154daf36f68f
amazingbutuseless <cindian@amazingbutuseless.net>
2020-12-09 23:01:37 +0900

    로고에 마우스오버시 랜덤하게 고양이 이모지를 표시하는 기능 추가

--- collector/src/components/Logo/index.tsx
+++ collector/src/components/Logo/index.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import styled from '@emotion/styled';
 
 const LogoWrapper = styled.h1`
-  text-align: center;
+  position: relative;
+
+  margin: 0;
+  padding: 0;
+  width: 6.4rem;
+  height: 6.4rem;
+
   font-size: 3.2rem;
+  cursor: pointer;
 
   span {
     display: none;
   }
+
+  &:after {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    content: '😺';
+  }
+
+  &:hover:after {
+    content: '${({ randomCat }) => randomCat}';
+  }
 `;
 
 export default function Logo() {
+  const [randomCat, setRandomCat] = useState('');
+
+  const cats = ['😸', '😹', '😻', '😼', '😽'];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRandomCat(cats[Math.floor(Math.random() * cats.length)]);
+    }, 2000);
+  }, [randomCat]);
+
   return (
-    <LogoWrapper>
-      <span>ijustwannasee</span>😺
+    <LogoWrapper randomCat={randomCat}>
+      <span>ijustwannaseewonwoo</span>
     </LogoWrapper>
   );
 }

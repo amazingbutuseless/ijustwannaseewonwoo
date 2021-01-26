87495321a484d0d8885ed69134a693a1de0482fa
amazingbutuseless <cindian@amazingbutuseless.net>
2021-01-26 20:25:47 +0900

    Video fetch 추가

---
+++ collector/src/actions/video.ts
@@ -0,0 +1,97 @@
+import {
+  createSlice,
+  createAsyncThunk,
+  createEntityAdapter,
+  createSelector,
+} from '@reduxjs/toolkit';
+
+import { client } from './apiClient';
+import { selectAllVideos } from './videos';
+
+interface SceneItem {
+  thumbnails: string;
+  start: string;
+  end: string;
+}
+
+export interface VideoItem {
+  id: string;
+  channelId: string;
+  videoId: string;
+  title: string;
+  publishedAt: string;
+  thumbnail: {
+    url: string;
+  };
+  scenes: SceneItem[];
+}
+
+const videoAdapter = createEntityAdapter<VideoItem>({
+  selectId: (video) => video.videoId,
+});
+
+const initialState = videoAdapter.getInitialState({
+  status: 'idle',
+  error: null,
+});
+
+const query = `
+query video($videoId: ID!) {
+  video(id: $videoId) {
+    id
+    channelId
+    videoId
+    title
+    publishedAt
+    thumbnail(size: high) {
+      url
+    }
+    scenes {
+      thumbnails
+      start
+      end
+    }
+  }
+}
+`;
+
+interface VideoFetcherProps {
+  videoId: string;
+}
+
+export const fetchVideo = createAsyncThunk(
+  'video/fetchVideo',
+  async ({ videoId }: VideoFetcherProps) => {
+    const response = await client.graphql({
+      query,
+      variables: {
+        videoId,
+      },
+    });
+
+    return response.data.video;
+  }
+);
+
+const VideoSlice = createSlice({
+  name: 'video',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchVideo.pending]: (state, action) => {
+      state.status = 'loading';
+    },
+    [fetchVideo.fulfilled]: (state, action) => {
+      state.status = 'succeeded';
+      videoAdapter.upsertOne(state, action.payload);
+    },
+    [fetchVideo.rejected]: (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    },
+  },
+});
+
+export default VideoSlice.reducer;
+
+export const { selectById: selectVideoById } = videoAdapter.getSelectors((state) => state.video);
--- collector/src/store.ts
+++ collector/src/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import ChannelsSlice from './actions/channels';
 import VideosSlice from './actions/videos';
+import VideoSlice from './actions/video';
 
 const store = configureStore({
   reducer: {
     channels: ChannelsSlice,
     videos: VideosSlice,
+    video: VideoSlice,
   },
 });
 

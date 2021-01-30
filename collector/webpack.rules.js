d71d55756fd2a121c89399b74fb911658578cd8a
amazingbutuseless <cindian@amazingbutuseless.net>
2021-01-30 21:39:32 +0900

    Component를 styled component 안에서 selector로 사용하기 위해 webpack의 typescript 룰을 ts-loader 대신 babel-loader를 사용하도록 수정

--- collector/webpack.rules.js
+++ collector/webpack.rules.js
@@ -17,11 +17,10 @@ module.exports = [
   {
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
-    use: {
-      loader: 'ts-loader',
-      options: {
-        transpileOnly: true
-      }
-    }
+    use: [
+      {
+        loader: 'babel-loader',
+      },
+    ],
   },
 ];

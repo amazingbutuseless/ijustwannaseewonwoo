de93c191f2b35dde4b5757e2ff3d79396ff33f84
amazingbutuseless <cindian@amazingbutuseless.net>
2021-05-01 16:09:17 +0900

    랜딩 페이지 수정

--- collector/src/components/UserSignIn.style.ts
+++ collector/src/components/UserSignIn.style.ts
@@ -11,3 +11,14 @@ export const UserSignInWrapper = styled.div`
   background-color: rgba(0, 0, 0, 0.4);
   place-content: center;
 `;
+
+export const ServiceSubTitle = styled.p`
+  align-self: center;
+
+  margin: 0;
+  margin-bottom: 0.8rem;
+  padding: 0;
+
+  font-size: 1.2rem;
+  color: var(--dark-orchid);
+`;
--- collector/src/components/UserSignIn.tsx
+++ collector/src/components/UserSignIn.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { UserSignInWrapper } from './UserSignIn.style';
+import { UserSignInWrapper, ServiceSubTitle } from './UserSignIn.style';
 
 import LogoImage from './LogoImage';
 import GoogleSignInButton from './GoogleSignInButton';
+import LandingVideo from './LandingVideo';
 
 interface UserSignInProps {
   onClick: () => void;
@@ -12,9 +13,10 @@ interface UserSignInProps {
 export default function UserSignIn({ onClick }: UserSignInProps) {
   return (
     <>
-      <video muted autoPlay style={{ objectFit: 'cover' }}></video>
+      <LandingVideo />
       <UserSignInWrapper>
         <LogoImage />
+        <ServiceSubTitle>원우가 나오는 유튜브 비디오 장면 모아보기</ServiceSubTitle>
         <GoogleSignInButton type="button" onClick={onClick} aria-label="Sign in with Google" />
       </UserSignInWrapper>
     </>

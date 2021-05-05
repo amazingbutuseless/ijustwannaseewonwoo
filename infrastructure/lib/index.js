659983ba75d63aaafb674862019de4780f45d775
amazingbutuseless <cindian@amazingbutuseless.net>
2021-05-05 20:16:46 +0900

    :wrench: Cognito 유저 권한 수정
    Authorized User 권한에서 DynamoDB 관련 권한을 삭제(Lambda function의 권한으로 이전)하고, API Gateway endpoint를 invoke 할 수 있는 권한을 추가

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -3,12 +3,9 @@ import S3Stack from './S3Stack';
 import CognitoStack from './CognitoStack';
 
 export default function main(app) {
-  const table = new DynamoDBStack(app, 'dynamodb');
+  new DynamoDBStack(app, 'dynamodb');
   const s3 = new S3Stack(app, 's3');
   new CognitoStack(app, 'cognito', {
-    tableArn:
-      table.tableArn ||
-      'arn:aws:dynamodb:ap-northeast-2:524717148731:table/ijustwannaseewonwoo-dev-db',
     bucketArn: s3.bucketArn || 'arn:aws:s3:::ijustwannaseewonwoo-training-set',
   });
 }

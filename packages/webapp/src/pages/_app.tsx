211d192957d26c7af290ad3958dc477673cffe69
amazingbutuseless <cindian@amazingbutuseless.net>
2022-01-03 22:14:27 +0900

    style: material ui 라이브러리 추가

--- packages/webapp/src/pages/_app.tsx
+++ packages/webapp/src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
+import { ThemeProvider } from '@material-ui/core';
 
 import i18n from '../i18n';
+import theme from './theme';
+import './global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   const { locale } = useRouter();
 
   i18n.changeLanguage(locale);
 
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
